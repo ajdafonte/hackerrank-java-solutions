ba057a52cbbfe73ec64753ddf044c797e2b3a57c
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2019-01-29 16:06:42 +0100

    Solve Exception Handling exercises

--- java-language/src/main/java/pt/caires/hackerrank/exception_handling/ExceptionHandling.java
+++ java-language/src/main/java/pt/caires/hackerrank/exception_handling/ExceptionHandling.java
@@ -1,8 +1,46 @@
 package pt.caires.hackerrank.exception_handling;
 
-/**
- * TODO
- */
+import java.util.Scanner;
+
+
 public class ExceptionHandling
 {
+    private static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(final String[] args)
+    {
+        while (in.hasNextInt())
+        {
+            final int n = in.nextInt();
+            final int p = in.nextInt();
+
+            try
+            {
+                System.out.println(my_calculator.power(n, p));
+            }
+            catch (final Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+    }
+
+    private static class MyCalculator
+    {
+        int power(final int n, final int p) throws Exception
+        {
+            if (n < 0 || p < 0)
+            {
+                throw new Exception("n or p should not be negative.");
+            }
+
+            if (n == 0 && p == 0)
+            {
+                throw new Exception("n and p should not be zero.");
+            }
+
+            return (int) Math.pow(n, p);
+        }
+    }
 }
--- java-language/src/main/java/pt/caires/hackerrank/exception_handling/TryCatch.java
+++ java-language/src/main/java/pt/caires/hackerrank/exception_handling/TryCatch.java
@@ -1,8 +1,60 @@
 package pt.caires.hackerrank.exception_handling;
 
-/**
- * TODO
- */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 public class TryCatch
 {
+    public static void main(final String[] args)
+    {
+        try (final Scanner sc = new Scanner(System.in))
+        {
+            final String x = sc.next();
+            final String y = sc.next();
+
+            System.out.println(specialDivision(x, y));
+        }
+        catch (final ArithmeticException | InputMismatchException e)
+        {
+            System.out.println(e.toString());
+        }
+    }
+
+    private static Long specialDivision(final String x, final String y) throws ArithmeticException, InputMismatchException
+    {
+        final Long numX = convertToLong(x);
+        final Long numY = convertToLong(y);
+        if (is32BitSigned(numX) && is32BitSigned(numY))
+        {
+            if (numY == 0)
+            {
+                throw new ArithmeticException("/ by zero");
+            }
+        }
+        else
+        {
+            throw new InputMismatchException();
+        }
+
+        return numX / numY;
+    }
+
+    private static Long convertToLong(final String x) throws InputMismatchException
+    {
+        try
+        {
+            return Long.valueOf(x);
+        }
+        catch (final NumberFormatException e)
+        {
+            throw new InputMismatchException();
+        }
+
+    }
+
+    private static boolean is32BitSigned(final Long x)
+    {
+        return x >= 0 && x < Integer.MAX_VALUE;
+    }
 }
