59c4340948cbf77f2625f19a325f9a820ce30751
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2021-11-29 19:24:12 +0100

    Reformat code

--- java-language/src/main/java/pt/caires/hackerrank/advanced/MD5.java
+++ java-language/src/main/java/pt/caires/hackerrank/advanced/MD5.java
@@ -8,39 +8,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-/**
- *
- */
-public class MD5
-{
+public class MD5 {
+
     private static final int MIN_SIZE = 6;
     private static final int MAX_SIZE = 20;
     private static final String COMPLIANT_PATTERN = "^[A-Za-z0-9]+$";
 
-    public static void main(final String[] args)
-    {
-        try (final Scanner sc = new Scanner(System.in))
-        {
+    public static void main(final String[] args) {
+        try (final Scanner sc = new Scanner(System.in)) {
             final String input = sc.nextLine();
             final String result = handleMd5Hash(input);
             System.out.println(result);
-        }
-        catch (final NoSuchAlgorithmException e)
-        {
+        } catch (final NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
     }
 
-    static String handleMd5Hash(final String input) throws NoSuchAlgorithmException
-    {
+    static String handleMd5Hash(final String input) throws NoSuchAlgorithmException {
         final int inputSize = input.length();
         final String result = "Constraint Violated";
-        if (inputSize >= MIN_SIZE && inputSize <= MAX_SIZE)
-        {
+        if (inputSize >= MIN_SIZE && inputSize <= MAX_SIZE) {
             final Pattern pattern = Pattern.compile(COMPLIANT_PATTERN);
             final Matcher matcher = pattern.matcher(input);
-            if (matcher.matches())
-            {
+            if (matcher.matches()) {
                 return getMd5Hash(input);
             }
         }
@@ -48,11 +38,11 @@ public class MD5
         return result;
     }
 
-    private static String getMd5Hash(final String input) throws NoSuchAlgorithmException
-    {
+    private static String getMd5Hash(final String input) throws NoSuchAlgorithmException {
         final MessageDigest md = MessageDigest.getInstance("MD5");
         final byte[] hashBytes = md.digest(input.getBytes());
         final BigInteger bi = new BigInteger(1, hashBytes);
         return bi.toString(16);
     }
+
 }
