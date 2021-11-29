e687c039827c5811c43d1b84dc438889c0e3964c
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2021-11-29 19:22:59 +0100

    Add JUnit5 support for 'java-language' module
    - Define JUnit5 test for MD5 class

--- java-language/src/test/java/pt/caires/hackerrank/advanced/MD5Test.java
+++ java-language/src/test/java/pt/caires/hackerrank/advanced/MD5Test.java
@@ -1,48 +1,48 @@
 package pt.caires.hackerrank.advanced;
 
-import javafx.util.Pair;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- *
- */
-public class MD5Test {
+class MD5Test {
 
-    public static void main(final String[] args) throws NoSuchAlgorithmException {
-        int failures = 0;
-
-        final Pair<String, String>[] testCases = new Pair[]{
-                new Pair("HelloWorld", "68e109f0f40ca72a15e05cc22786f8e6"),
-                new Pair("Javarmi123", "2da2d1e0ce7b4951a858ed2d547ef485"),
-                new Pair("zepassarinho", "2cd1be5eef5313196d4a5450d10cab39"),
-                new Pair(" ", "Constraint Violated"),
-                new Pair("qwertyuioplkjhgfdsazxc", "Constraint Violated"),
-                new Pair("cr7@goal.com", "Constraint Violated")
-        };
-
-        for (final Pair<String, String> testCase : testCases) {
-            final String input = testCase.getKey();
-            final String expectedResult = testCase.getValue();
-            failures += executeTest(input, expectedResult);
-        }
-
-        if (failures > 0) {
-            throw new RuntimeException("Incurred " + failures + " failures while testing " + MD5.class.getSimpleName());
-        } else {
-            System.out.println("All tests are OK!!");
-        }
+    private static Stream<Arguments> invalidMd5Hashes() {
+        return Stream.of(
+                Arguments.of(" "),
+                Arguments.of("qwertyuioplkjhgfdsazxc"),
+                Arguments.of("cr7@goal.com"));
     }
 
-    private static int executeTest(final String input, final String expectedResult) throws NoSuchAlgorithmException {
-        final String result = MD5.handleMd5Hash(input);
-        if (!result.equals(expectedResult)) {
-            System.err.println("Expected result '" + expectedResult + "' , got the following result '" + result + "' ");
-            return 1;
-        }
+    private static Stream<Arguments> validMd5Hashes() {
+        return Stream.of(
+                Arguments.of("HelloWorld", "68e109f0f40ca72a15e05cc22786f8e6"),
+                Arguments.of("Javarmi123", "2da2d1e0ce7b4951a858ed2d547ef485"),
+                Arguments.of("zepassarinho", "2cd1be5eef5313196d4a5450d10cab39"));
+    }
 
-        return 0;
+    @ParameterizedTest
+    @MethodSource("validMd5Hashes")
+    void should_generate_md5_hash(String input, String expected) throws NoSuchAlgorithmException {
+        String result = MD5.handleMd5Hash(input);
+
+        assertThat(result)
+                .isNotBlank()
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidMd5Hashes")
+    void should_not_generate_md5_hash(String input) throws NoSuchAlgorithmException {
+        String result = MD5.handleMd5Hash(input);
+
+        assertThat(result)
+                .isNotBlank()
+                .isEqualTo("Constraint Violated");
     }
 
 }
