222a034fa4ee8872a01851c2580df8e8f6a44fbd
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2021-11-29 22:20:35 +0100

    Define JUnit5 tests and reformat src code

--- java-language/src/main/java/pt/caires/hackerrank/datastructures/JavaStack.java
+++ java-language/src/main/java/pt/caires/hackerrank/datastructures/JavaStack.java
@@ -4,53 +4,40 @@ import java.util.Scanner;
 import java.util.Stack;
 
 
-/**
- *
- */
-public class JavaStack
-{
+public class JavaStack {
+
     private static final char[] START_DELIMITERS = {'(', '[', '{'};
     private static final char[] END_DELIMITERS = {')', ']', '}'};
 
-    public static void main(final String[] args)
-    {
+    public static void main(final String[] args) {
         final Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNext())
-        {
+        while (sc.hasNext()) {
             final String input = sc.next();
             System.out.println(isBalanced(input));
         }
 
     }
 
-    static Boolean isBalanced(final String input)
-    {
+    static Boolean isBalanced(final String input) {
         final Stack<Character> stackChars = new Stack<>();
 
         // some initial checks
-        if (input.length() % 2 != 0)
-        {
+        if (input.length() % 2 != 0) {
             return false;
         }
 
-        for (int i = 0; i < input.length(); i++)
-        {
+        for (int i = 0; i < input.length(); i++) {
             final char currChar = input.charAt(i);
-            if (isEndDelimiter(currChar) && stackChars.empty())
-            {
+            if (isEndDelimiter(currChar) && stackChars.empty()) {
                 return false;
             }
 
-            if (isStartDelimiter(currChar))
-            {
+            if (isStartDelimiter(currChar)) {
                 stackChars.push(currChar);
-            }
-            else if (isEndDelimiter(currChar))
-            {
+            } else if (isEndDelimiter(currChar)) {
                 final char lastChar = stackChars.pop();
-                if (!checkIfDelimitersMatches(lastChar, currChar))
-                {
+                if (!checkIfDelimitersMatches(lastChar, currChar)) {
                     return false;
                 }
             }
@@ -59,34 +46,28 @@ public class JavaStack
         return stackChars.empty();
     }
 
-    private static boolean checkIfDelimitersMatches(final char startChar, final char endChar)
-    {
+    private static boolean checkIfDelimitersMatches(final char startChar, final char endChar) {
         return (startChar == '(' && endChar == ')') ||
-            (startChar == '[' && endChar == ']') ||
-            (startChar == '{' && endChar == '}');
+                (startChar == '[' && endChar == ']') ||
+                (startChar == '{' && endChar == '}');
     }
 
-    private static boolean isEndDelimiter(final char charDelimiter)
-    {
-        for (final char openDelimiter : END_DELIMITERS)
-        {
-            if (openDelimiter == charDelimiter)
-            {
+    private static boolean isEndDelimiter(final char charDelimiter) {
+        for (final char openDelimiter : END_DELIMITERS) {
+            if (openDelimiter == charDelimiter) {
                 return true;
             }
         }
         return false;
     }
 
-    private static boolean isStartDelimiter(final char charDelimiter)
-    {
-        for (final char openDelimiter : START_DELIMITERS)
-        {
-            if (openDelimiter == charDelimiter)
-            {
+    private static boolean isStartDelimiter(final char charDelimiter) {
+        for (final char openDelimiter : START_DELIMITERS) {
+            if (openDelimiter == charDelimiter) {
                 return true;
             }
         }
         return false;
     }
+
 }
--- java-language/src/test/java/pt/caires/hackerrank/datastructures/JavaStackTest.java
+++ java-language/src/test/java/pt/caires/hackerrank/datastructures/JavaStackTest.java
@@ -1,50 +1,23 @@
 package pt.caires.hackerrank.datastructures;
 
-import javafx.util.Pair;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
-/**
- *
- */
-public class JavaStackTest {
+class JavaStackTest {
 
-    public static void main(final String[] args) {
-        int failures = 0;
-
-        final Pair<String, Boolean>[] testCases = new Pair[]{
-                new Pair("{}()", Boolean.TRUE),
-                new Pair("[{()}]", Boolean.TRUE),
-                new Pair("({()})", Boolean.TRUE),
-                new Pair("[]", Boolean.TRUE),
-                new Pair("{}(", Boolean.FALSE),
-                new Pair("({)}", Boolean.FALSE),
-                new Pair("[[", Boolean.FALSE),
-                new Pair("}{", Boolean.FALSE)
-        };
-
-        for (final Pair<String, Boolean> testCase : testCases) {
-            final String input = testCase.getKey();
-            final Boolean expectedResult = testCase.getValue();
-            failures += executeTest(input, expectedResult);
-        }
-
-        if (failures > 0) {
-            throw new RuntimeException(
-                    "Incurred " + failures + " failures while testing " + JavaStack.class.getSimpleName());
-        } else {
-            System.out.println("All tests are OK!!");
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {"{}()", "[{()}]", "({()})", "[]"})
+    void should_be_balanced(String input) {
+        assertThat(JavaStack.isBalanced(input)).isTrue();
     }
 
-    private static int executeTest(final String input, final Boolean expectedResult) {
-        final Boolean result = JavaStack.isBalanced(input);
-        if (!result.equals(expectedResult)) {
-            System.err.println("For input '" + input + "', got the following result '" + result + "'. Expected result '"
-                                       + expectedResult + "'.");
-            return 1;
-        }
-
-        return 0;
+    @ParameterizedTest
+    @ValueSource(strings = {"{}(", "({)}", "[[", "}{"})
+    void should_not_be_balanced(String input) {
+        assertThat(JavaStack.isBalanced(input)).isFalse();
     }
 
 }
