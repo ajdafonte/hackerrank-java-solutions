e0656218dfcc1ff6dad7a0b043183117d964013c
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2021-11-29 19:36:34 +0100

    Define JUnit5 tests for SHA256 class

--- java-language/src/main/java/pt/caires/hackerrank/advanced/SHA256.java
+++ java-language/src/main/java/pt/caires/hackerrank/advanced/SHA256.java
@@ -7,39 +7,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-/**
- *
- */
-public class SHA256
-{
+public class SHA256 {
+
     private static final int MIN_SIZE = 6;
     private static final int MAX_SIZE = 20;
     private static final String COMPLIANT_PATTERN = "^[A-Za-z0-9]+$";
 
-    public static void main(final String[] args)
-    {
-        try (final Scanner sc = new Scanner(System.in))
-        {
+    public static void main(final String[] args) {
+        try (final Scanner sc = new Scanner(System.in)) {
             final String input = sc.nextLine();
             final String result = handleSha256Hash(input);
             System.out.println(result);
-        }
-        catch (final NoSuchAlgorithmException e)
-        {
+        } catch (final NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
     }
 
-    static String handleSha256Hash(final String input) throws NoSuchAlgorithmException
-    {
+    static String handleSha256Hash(final String input) throws NoSuchAlgorithmException {
         final int inputSize = input.length();
         final String result = "Constraint Violated";
-        if (inputSize >= MIN_SIZE && inputSize <= MAX_SIZE)
-        {
+        if (inputSize >= MIN_SIZE && inputSize <= MAX_SIZE) {
             final Pattern pattern = Pattern.compile(COMPLIANT_PATTERN);
             final Matcher matcher = pattern.matcher(input);
-            if (matcher.matches())
-            {
+            if (matcher.matches()) {
                 return getSha256Hash(input);
             }
         }
@@ -47,15 +37,14 @@ public class SHA256
         return result;
     }
 
-    private static String getSha256Hash(final String input) throws NoSuchAlgorithmException
-    {
+    private static String getSha256Hash(final String input) throws NoSuchAlgorithmException {
         final MessageDigest md = MessageDigest.getInstance("SHA-256");
         final byte[] hashBytes = md.digest(input.getBytes());
         final StringBuilder result = new StringBuilder();
-        for (final byte b : hashBytes)
-        {
+        for (final byte b : hashBytes) {
             result.append(String.format("%02x", b));
         }
         return result.toString();
     }
+
 }
--- java-language/src/test/java/pt/caires/hackerrank/advanced/SHA256Test.java
+++ java-language/src/test/java/pt/caires/hackerrank/advanced/SHA256Test.java
@@ -1,51 +1,51 @@
 package pt.caires.hackerrank.advanced;
 
-import javafx.util.Pair;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
-/**
- *
- */
-public class SHA256Test {
+class SHA256Test {
 
-    public static void main(final String[] args) throws NoSuchAlgorithmException {
-        int failures = 0;
-
-        final Pair<String, String>[] testCases = new Pair[]{
-                new Pair("HelloWorld", "872e4e50ce9990d8b041330c47c9ddd11bec6b503ae9386a99da8584e9bb12c4"),
-                new Pair("Javarmi123", "f1d5f8d75bb55c777207c251d07d9091dc10fe7d6682db869106aacb4b7df678"),
-                new Pair("zepassarinho", "65379e13682aa4b6b96e586dd893560b30ec43b9a407becde7c1a71f0d45bd6b"),
-                new Pair(" ", "Constraint Violated"),
-                new Pair("qwertyuioplkjhgfdsazxc", "Constraint Violated"),
-                new Pair("cr7@goal.com", "Constraint Violated"),
-                new Pair("K1t4fo0V", "0a979e43f4874eb24b740c0157994e34636eed0425688161cc58e8b26b1dcf4e"),
-                new Pair("yxPX0fbIKHvjHo180", "074690988f8d3e8e45840f7502efd2edde63a88c299a88e0923e7ed4abec1835")
-        };
-
-        for (final Pair<String, String> testCase : testCases) {
-            final String input = testCase.getKey();
-            final String expectedResult = testCase.getValue();
-            failures += executeTest(input, expectedResult);
-        }
-
-        if (failures > 0) {
-            throw new RuntimeException(
-                    "Incurred " + failures + " failures while testing " + SHA256.class.getSimpleName());
-        } else {
-            System.out.println("All tests are OK!!");
-        }
+    private static Stream<Arguments> validSha256Hashes() {
+        return Stream.of(
+                Arguments.of("HelloWorld", "872e4e50ce9990d8b041330c47c9ddd11bec6b503ae9386a99da8584e9bb12c4"),
+                Arguments.of("Javarmi123", "f1d5f8d75bb55c777207c251d07d9091dc10fe7d6682db869106aacb4b7df678"),
+                Arguments.of("zepassarinho", "65379e13682aa4b6b96e586dd893560b30ec43b9a407becde7c1a71f0d45bd6b"),
+                Arguments.of("K1t4fo0V", "0a979e43f4874eb24b740c0157994e34636eed0425688161cc58e8b26b1dcf4e"),
+                Arguments.of("yxPX0fbIKHvjHo180", "074690988f8d3e8e45840f7502efd2edde63a88c299a88e0923e7ed4abec1835"));
     }
 
-    private static int executeTest(final String input, final String expectedResult) throws NoSuchAlgorithmException {
-        final String result = SHA256.handleSha256Hash(input);
-        if (!result.equals(expectedResult)) {
-            System.err.println("Expected result '" + expectedResult + "' , got the following result '" + result + "' ");
-            return 1;
-        }
+    private static Stream<Arguments> invalidSha256Hashes() {
+        return Stream.of(
+                Arguments.of(" ", "Constraint Violated"),
+                Arguments.of("qwertyuioplkjhgfdsazxc", "Constraint Violated"),
+                Arguments.of("cr7@goal.com", "Constraint Violated"));
+    }
 
-        return 0;
+    @ParameterizedTest
+    @MethodSource("validSha256Hashes")
+    void should_generate_sha256_hash(String input, String expected) throws NoSuchAlgorithmException {
+        String result = SHA256.handleSha256Hash(input);
+
+        assertThat(result)
+                .isNotBlank()
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidSha256Hashes")
+    void should_not_generate_sha256_hash(String input) throws NoSuchAlgorithmException {
+        String result = SHA256.handleSha256Hash(input);
+
+        assertThat(result)
+                .isNotBlank()
+                .isEqualTo("Constraint Violated");
     }
 
 }
