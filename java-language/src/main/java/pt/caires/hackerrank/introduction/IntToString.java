6c7c2c137df51af2813c549c18b2c863289e8350
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2021-12-02 16:03:19 +0100

    Reformat code

--- java-language/src/main/java/pt/caires/hackerrank/introduction/IntToString.java
+++ java-language/src/main/java/pt/caires/hackerrank/introduction/IntToString.java
@@ -4,62 +4,49 @@ import java.security.Permission;
 import java.util.Scanner;
 
 
-/**
- *
- */
-public class IntToString
-{
-    public static void main(final String[] args)
-    {
+public class IntToString {
+
+    public static void main(final String[] args) {
 
         DoNotTerminate.forbidExit();
 
-        try
-        {
+        try {
             final Scanner in = new Scanner(System.in);
             final int n = in.nextInt();
             in.close();
             final String s = String.valueOf(n);
 
-            if (n == Integer.parseInt(s))
-            {
+            if (n == Integer.parseInt(s)) {
                 System.out.println("Good job");
-            }
-            else
-            {
+            } else {
                 System.out.println("Wrong answer.");
             }
-        }
-        catch (final DoNotTerminate.ExitTrappedException e)
-        {
+        } catch (final DoNotTerminate.ExitTrappedException e) {
             System.out.println("Unsuccessful Termination!!");
         }
     }
+
 }
 
 //The following class will prevent you from terminating the code using exit(0)!
-class DoNotTerminate
-{
+class DoNotTerminate {
 
-    static class ExitTrappedException extends SecurityException
-    {
-
-        private static final long serialVersionUID = 1;
-    }
-
-    static void forbidExit()
-    {
-        final SecurityManager securityManager = new SecurityManager()
-        {
+    static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
             @Override
-            public void checkPermission(final Permission permission)
-            {
-                if (permission.getName().contains("exitVM"))
-                {
+            public void checkPermission(final Permission permission) {
+                if (permission.getName().contains("exitVM")) {
                     throw new ExitTrappedException();
                 }
             }
         };
         System.setSecurityManager(securityManager);
     }
+
+    static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1;
+
+    }
+
 }
--- java-language/src/main/java/pt/caires/hackerrank/introduction/Loops1.java
+++ java-language/src/main/java/pt/caires/hackerrank/introduction/Loops1.java
@@ -4,20 +4,18 @@ import java.util.Scanner;
 import java.util.stream.IntStream;
 
 
-/**
- *
- */
-public class Loops1
-{
+public class Loops1 {
+
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(final String[] args)
-    {
+    public static void main(final String[] args) {
         final int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        IntStream.rangeClosed(1, 10).forEach(num -> System.out.println(N + " x " + num + " = " + N * num));
+        IntStream.rangeClosed(1, 10)
+                .forEach(num -> System.out.println(N + " x " + num + " = " + N * num));
 
         scanner.close();
     }
+
 }
