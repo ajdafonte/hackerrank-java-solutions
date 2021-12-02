24e237f2a7e35f1e3d6633bd6876a321fac69037
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2021-12-02 17:28:17 +0100

    Reformat code and add remarks to refactor code

--- java-language/src/main/java/pt/caires/hackerrank/bignumber/BigDecimalIntro.java
+++ java-language/src/main/java/pt/caires/hackerrank/bignumber/BigDecimalIntro.java
@@ -6,34 +6,29 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 
-/**
- *
- */
-class BigDecimalIntro
-{
+// TODO: 02/12/2021 Refactor this approach
+class BigDecimalIntro {
+
     // Other option - for other approach to the problem
-    private static String[] orderedRealNumberStrings(final String[] numStrs)
-    {
+    private static String[] orderedRealNumberStrings(final String[] numStrs) {
         final BigDecimal[] decimals = Arrays.stream(numStrs)
-            .filter(x -> x != null)
-            .map(BigDecimal::new)
-            .sorted(Comparator.reverseOrder())
-            .toArray(BigDecimal[]::new);
+                .filter(x -> x != null)
+                .map(BigDecimal::new)
+                .sorted(Comparator.reverseOrder())
+                .toArray(BigDecimal[]::new);
         Arrays.stream(decimals).forEach(System.out::println);
 
         return Arrays.stream(decimals)
-            .map(BigDecimal::toPlainString)
-            .toArray(String[]::new);
+                .map(BigDecimal::toPlainString)
+                .toArray(String[]::new);
     }
 
-    public static void main(final String[] args)
-    {
+    public static void main(final String[] args) {
         //Input
         final Scanner sc = new Scanner(System.in);
         final int n = sc.nextInt();
         String[] s = new String[n + 2];
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             s[i] = sc.next();
         }
         sc.close();
@@ -44,18 +39,18 @@ class BigDecimalIntro
         // - Each number must be printed in the exact same format as it was read from stdin
 //        s = orderedRealNumberStrings(s);
         s = Arrays.stream(s)
-            .filter(x -> x != null)
-            .sorted((o1, o2) -> {
-                BigDecimal decimal1 = new BigDecimal(o1);
-                BigDecimal decimal2 = new BigDecimal(o2);
-                return decimal2.compareTo(decimal1);
-            })
-            .toArray(String[]::new);
+                .filter(x -> x != null)
+                .sorted((o1, o2) -> {
+                    BigDecimal decimal1 = new BigDecimal(o1);
+                    BigDecimal decimal2 = new BigDecimal(o2);
+                    return decimal2.compareTo(decimal1);
+                })
+                .toArray(String[]::new);
 
         //Output
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             System.out.println(s[i]);
         }
     }
+
 }
--- java-language/src/main/java/pt/caires/hackerrank/bignumber/BigIntegerIntro.java
+++ java-language/src/main/java/pt/caires/hackerrank/bignumber/BigIntegerIntro.java
@@ -4,19 +4,15 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 
-/**
- *
- */
-public class BigIntegerIntro
-{
-    public static void main(final String[] args)
-    {
-        try (final Scanner sc = new Scanner(System.in))
-        {
+public class BigIntegerIntro {
+
+    public static void main(final String[] args) {
+        try (final Scanner sc = new Scanner(System.in)) {
             final BigInteger bi1 = sc.nextBigInteger();
             final BigInteger bi2 = sc.nextBigInteger();
             System.out.println(bi1.add(bi2));
             System.out.println(bi1.multiply(bi2));
         }
     }
+
 }
